f62d15a685c935e489397d587026310c56c285b5
Mriitika150 <mrittikajahan@iut-dhaka.edu>
2026-05-09 23:09:55 +0600

    Refining the logic of the game. Add cell class

---
+++ Cell.java
@@ -0,0 +1,52 @@
+public class Cell {
+    private boolean isMine;
+    private boolean isRevealed;
+    private boolean isFlagged ;
+    private int adjacentMines;
+
+    public Cell(){
+        this.isMine = false;
+        this.isRevealed = false;
+        this.isFlagged = false;
+        this.adjacentMines = 0;
+    }
+
+    public void reveal(){
+        if(!isFlagged) {
+            isFlagged = true;
+        }
+    }
+
+    public void toggleFlag(){
+        if(!isRevealed){
+            isFlagged=!isFlagged;
+        }
+    }
+
+    public void reset(){
+        isMine=false;
+        isRevealed=false;
+        isFlagged=false;
+        adjacentMines=0;
+    }
+
+    public boolean isMine(){
+        return isMine;
+    }
+    public boolean isRevealed(){
+        return isRevealed;
+    }
+    public boolean isFlagged(){
+        return isFlagged;
+    }
+    public int getAdjacentMines(){
+        return adjacentMines;
+    }
+
+    public void setMine(boolean mine){
+        this.isMine=mine;
+    }
+    public void setAdjacentMines(int count){
+        this.adjacentMines=count;
+    }
+}
